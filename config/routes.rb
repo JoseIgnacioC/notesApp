8c1618e35c178e86d7d691343f47d08d3473a9df
Jose Cortes <jose.cortes@usach.cl>
2017-06-07 18:56:43 -0400

    EDIT: Eliminar rutas innecesarias de los recursos

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :labels
+  
+  resources :labels, except: [:index, :show, :new, :edit]
   devise_for :users
-  resources :notes, except: [:show, :new, :edit ]
+  
+  resources :notes, except: [:edit, :new ] do
+  	resources :commenters
+  end
 	
   root :to => 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
