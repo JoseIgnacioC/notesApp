a413d289a0cb646edc3e2caa07cde05e6c14b118
Jose Cortes <jose.cortes@usach.cl>
2017-07-10 21:21:16 -0400

    ADD: Filtrar notas en base a los labels

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
 
   post 'notes/diHola', to: "notes#diHola"
-  
+  get 'notes/filter_labels/:id', to: "notes#filter_labels"
+
   resources :labels, except: [:index, :show, :new, :edit]
   devise_for :users
   
-  resources :notes, except: [:edit, :new ] do
+  resources :notes, except: [ :new ] do
   	resources :commenters
   end
 	
   root :to => 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
